22eb19f9dd167076182903d8f2985f129ffe671a
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-03-20 00:00:47 +0530

    delete product findbyIdAndRemove method mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -90,7 +90,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  //find ID ang delete this is inbuilt mongoose method
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log("DESTROYED PRODUCT");
       res.redirect("/admin/products");
